eb8a5de3e712cb0d6423ac60cf8de7e7e5eb31db
WahyuAprian-193040141 <63623188+WahyuAprian-193040141@users.noreply.github.com>
2020-05-14 20:13:17 +0700

    Tugas Kuliah Pertemuan 12

---
+++ kuliah/Pertemuan 12/latihan7b/php/login.php
@@ -0,0 +1,85 @@
+<?php 
+	session_start();
+	require 'functions.php';
+
+	// melakukan pengecekan apakah user suda melakukan login jika sudah redirect ke halaman admin
+	if (isset($_SESSION['username'])) {
+		header("Location: admin.php");
+		exit;
+	}
+
+	//login
+	if (isset($_POST['submit'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$cek_user = mysqli_query(koneksi(), "SELECT * FROM user WHERE username = '$username'");
+		// mencocokan username dan password
+		if (mysqli_num_rows($cek_user) > 0) {
+			$row = mysqli_fetch_assoc($cek_user);
+			if (password_verify($password, $row['password'])) {
+				$_SESSION['username'] = $_POST['username'];
+				$_SESSION['hash'] = hash('sha256', $row['id'], false);
+				
+				if (hash('sha256', $row['id']) == $_SESSION['hash']) {
+					header("Location: admin.php");
+					die;
+				}
+				header("Location: ../index.php");
+				die;
+			}
+		}
+		$error = true;
+	}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="../css/css2.css">
+</head>
+<body>
+	<h1>Login</h1>
+	<form action="" method="post">
+		<?php if (isset($error)) : ?>
+			<p class="error">Username atau Password salah</p>
+		<?php endif; ?>
+		<table border="0" cellpadding="6" cellspacing="0" class="tabel">
+			<tr>
+				<td>
+					<label for="username">Username</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="username">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="password">Password</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="password" name="password">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" class="tengah">
+					<input type="checkbox" name="remember">
+					<label for="remember">Remember me</label>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" class="tengah">
+					<button type="submit" name="submit" class="tombol">Login</button>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" class="tengah">
+					<p>Belum punya akun? Registrasi <a href="registrasi.php">disini</a></p>
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ kuliah/Pertemuan 12/latihan7c/php/tambah.php
@@ -0,0 +1,89 @@
+<?php 
+	session_start();
+	if (!isset($_SESSION["username"])) {
+		header("Location: login.php");
+		exit;
+	}
+
+	require 'functions.php';
+
+	if (isset($_POST['tambah'])) {
+		if (tambah($_POST) > 0) {
+			echo "<script>
+					alert('Data berhasil ditambahkan');
+					document.location.href = 'admin.php';
+					</script>";
+		} else {
+			echo "<script>
+					alert('Data gagal ditambahkan');
+					document.location.href = 'admin.php';
+					</script>";
+		}
+	}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Tambah Data</title>
+	<link rel="stylesheet" type="text/css" href="../css/css2.css">
+</head>
+<body>
+	<h3>Form Tambah Data Elektronik</h3>
+	<form action="" method="post">
+		<table border="0" cellpadding="6" cellspacing="0" class="tabel">
+			<tr>
+				<td>
+					<label for="foto">Foto</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="foto" id="foto" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="jenis">Jenis Barang</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="jenis" id="jenis" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="nama">Nama Barang</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="nama" id="nama" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="deskripsi">Deskripsi Produk</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="deskripsi" id="deskripsi" required>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="hargabaru">Harga Baru</label>
+				</td>
+				<td>:</td>
+				<td>
+					<input type="text" name="hargabaru" id="hargabaru" required>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" class="tengah">
+					<button type="submit" name="tambah" class="tombol">Tambah Data</button>
+					<button type="submit" class="tombol"><a href="../index.php" class="kembali">Kembali</a></button>
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
